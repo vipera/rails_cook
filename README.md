56d86923bb2a9179a173239fa36e7279e663c0ed
Marin Rukavina <marin@shinyshell.net>
2019-10-21 09:15:19 +0200

    Add logic for automatically determining Rails version
    Support the Rails version to be automatically inferred from the
    cookie's format, as far as possible.

--- README.md
+++ README.md
@@ -11,16 +11,38 @@ Want to reuse a session created by a Ruby on Rails application for an Erlang
 service, such as a web API or websocket server?
 
 ```erlang
-try rails_cook:verify_and_decrypt_cookie(rails5, <<"your rails secret">>, <<"Cookie payload">>) of
+try rails_cook:verify_and_decrypt(auto, <<"your rails secret">>, <<"Cookie payload">>) of
   Payload -> jiffy:decode(Payload) % or your other preferred deserialization
 catch
-  error:bad_sig -> "Invalid session cookie"
+  error:bad_sig -> "Invalid session cookie";
+  error:unknown_rails_version -> "Unable to determine Rails version"
 end
 ```
 
+The `auto` argument will ask the library to attempt to automatically infer the
+Rails version that created the cookie. Rails went though a couple of different
+cookie storage formats over the years (supported are 4 and 5.1, 3 or less is not
+supported). You can also specify the version yourself:
+
+```erlang
+rails_cook:verify_and_decrypt('rails-5.1', <<"your rails secret">>, <<"Cookie payload">>)
+```
+
+The utilized decryption/verification and HMAC digest algorithms are configurable,
+as they are in Rails, so if you changed the defaults in your Rails setup, you
+should be able to get this library to decrypt your cookie. Reasonable defaults
+are assumed:
+
+| Purpose                 | Rails 4     | Rails 5     |
+| ----------------------- |:-----------:|:-----------:|
+| HMAC digest             | SHA-1       | SHA-1       |
+| Decryption/Verification | AES-256-CBC | AES-256-GCM |
+
 ## To Do
 
-This library can get a whole lot better by understanding Rails 3 and 4 sessions,
-handling deserialization better, etc.
+This library currently makes no effort to actually deserialize the cookie
+payload. Whether it should do so, or leave that to you is being considered.
 
-The API is not stable, and may radically change with updates.
+Tests and better documentation is on the way.
+
+The API is not yet stable, and may change with updates.
--- src/rails_cook.erl
+++ src/rails_cook.erl
@@ -1,20 +1,42 @@
 %%% Rails cookie utilities
 
 -module(rails_cook).
--export([
-  verify_cookie/4,
-  decrypt_cookie/4,
-  verify_and_decrypt_cookie/3,
-  verify_and_decrypt_cookie/4
-]).
+-export([infer_rails_version/1,
+         verify/4,
+         decrypt/4,
+         verify_and_decrypt/3,
+         verify_and_decrypt/4]).
 
 %%% PUBLIC API
+
+-spec infer_rails_version(binary()) -> atom().
+
+%% Attempts to automatically determine the rails version from the cookie format
+infer_rails_version(Cookie) when is_binary(Cookie) ->
+  CookieParts = binary:split(http_uri:decode(Cookie), <<"--">>, [global]),
+  case erlang:length(CookieParts) - 1 of
+    1 -> 'rails-4';
+    2 -> 'rails-5.1';
+    _ -> unknown
+  end.
+
+-spec decrypt(atom(), binary(), binary(), proplists:proplist()) ->
+  binary().
+
+%% Automatically infers Rails version from the cookie format and decrypts the
+%% cookie
+decrypt(auto, RailsKey, Cookie, Properties) ->
+  RailsVersion = infer_rails_version(Cookie),
+  decrypt(RailsVersion, RailsKey, Cookie, Properties);
+
+decrypt(unknown, _RailsKey, _Cookie, _Properties) ->
+  erlang:error(unknown_rails_version);
 
 %% Since 5.1.0, Rails is leveraging AEAD (AES-256-GCM cipher) for faster
 %% encryption/decryption and verification, in a single step.
 %%
 %% <cookie-payload> ::= <cipher-text> "--" <cipher-iv> "--" <cipher-auth-tag>
-decrypt_cookie('rails-5.1', RailsKey, Cookie, Properties) ->
+decrypt('rails-5.1', RailsKey, Cookie, Properties) ->
   [B64Ciphertext, B64IV, B64AuthTag] = binary:split(http_uri:decode(Cookie), <<"--">>),
   Ciphertext = base64:decode(B64Ciphertext),
   IV = base64:decode(B64IV),
@@ -30,7 +52,7 @@ decrypt_cookie('rails-5.1', RailsKey, Cookie, Properties) ->
 %% where payload is encrypted with AES-256-CBC block cipher.
 %%
 %% <verified-payload> ::= <cipher-text> "--" <cipher-iv>
-decrypt_cookie('rails-4', RailsKey, Cookie, Properties) ->
+decrypt('rails-4', RailsKey, Cookie, Properties) ->
   [B64Ciphertext, B64IV] = binary:split(http_uri:decode(Cookie), <<"--">>),
   Ciphertext = base64:decode(B64Ciphertext),
   IV = base64:decode(B64IV),
@@ -41,15 +63,27 @@ decrypt_cookie('rails-4', RailsKey, Cookie, Properties) ->
   end,
   crypto:block_decrypt(CipherAlg, EncCookieKey, IV, Ciphertext).
 
+-spec verify(atom(), binary(), binary(), proplists:proplist()) ->
+  binary().
+
+%% Automatically infers Rails version from the cookie format and verifies the
+%% cookie
+verify(auto, RailsKey, Cookie, Properties) ->
+  RailsVersion = infer_rails_version(Cookie),
+  verify(RailsVersion, RailsKey, Cookie, Properties);
+
+verify(unknown, _RailsKey, _Cookie, _Properties) ->
+  erlang:error(unknown_rails_version);
+
 %% For Rails >= 5.1.0 A "null verifier" is used, i.e. no verification is done by
 %% hand, as it is handled by the cipher. The payload is base64 encoded.
-verify_cookie('rails-5.1', _RailsKey, Cookie, _Properties) ->
+verify('rails-5.1', _RailsKey, Cookie, _Properties) ->
   {ok, base64:decode(Cookie)};
 
 %% Rails < 5.1.0 uses a HMAC to prove the authenticity of a cookie payload
 %%
 %% <cookie-payload> ::= <verified-payload-b64> "--" <hmac>
-verify_cookie('rails-4', RailsKey, Cookie, Properties) ->
+verify('rails-4', RailsKey, Cookie, Properties) ->
   %% Rails 4 cookie is HMAC-signed at the outmost layer: PAYLOAD--SIGNATURE
   [CookieData, Digest] = binary:split(http_uri:decode(Cookie), <<"--">>),
   %% Verify that the signature is correct
@@ -67,16 +101,33 @@ verify_cookie('rails-4', RailsKey, Cookie, Properties) ->
       bad_sig
   end.
 
-verify_and_decrypt_cookie(RailsVersion, RailsKey, Cookie) ->
-  verify_and_decrypt_cookie(RailsVersion, RailsKey, Cookie, []).
+-spec verify_and_decrypt(atom(),
+                         binary(),
+                         binary(),
+                         proplists:proplist()) ->
+    binary().
 
-verify_and_decrypt_cookie(RailsVersion, RailsKey, Cookie, Properties) ->
-  case verify_cookie(RailsVersion, RailsKey, Cookie, Properties) of
+%% Automatically infers Rails version from the cookie format, then verifies and
+%% decrypts the cookie
+verify_and_decrypt(auto, RailsKey, Cookie, Properties) ->
+  RailsVersion = infer_rails_version(Cookie),
+  verify_and_decrypt(RailsVersion, RailsKey, Cookie, Properties);
+
+verify_and_decrypt(unknown, _RailsKey, _Cookie, _Properties) ->
+  erlang:error(unknown_rails_version);
+
+verify_and_decrypt(RailsVersion, RailsKey, Cookie, Properties) ->
+  case verify(RailsVersion, RailsKey, Cookie, Properties) of
     {ok, CookieData} ->
-      decrypt_cookie(RailsVersion, RailsKey, CookieData, Properties);
+      decrypt(RailsVersion, RailsKey, CookieData, Properties);
     Reason ->
       erlang:error(Reason)
   end.
+
+-spec verify_and_decrypt(atom(), binary(), binary()) -> binary().
+
+verify_and_decrypt(RailsVersion, RailsKey, Cookie) ->
+  verify_and_decrypt(RailsVersion, RailsKey, Cookie, []).
 
 %%% PRIVATE FUNCTIONS
 
